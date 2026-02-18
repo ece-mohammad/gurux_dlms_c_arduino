17521a4e9bb9d763ce667ef249b9bb633f7fc760
ece-mohammad <kuro.ece@gmail.com>
2026-02-18 14:30:12 +0200

    feat(library-structure): organized files acording to arduino library structure
    added library.properties, keywords.txt and moved src and include directories inside a new src directory, also added a gx_dlms_c.h header file as the library header file, that includes all other header files in src/src/include
    
    BREAKING CHANGE: makefile was moved into root src directory, static ibrary is built from there
    
    Signed-off-by: ece-mohammad <kuro.ece@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# gurux_dlms_c_arduino
-Unofficial Gurux DLMS.c arduino library
+# gx_dlms_c_arduino
+
+Unofficial Gurux DLMS.c arduino library. 
+
+## Introduction
+
+Gurux provides open-source Gurux.DLMS.c, a platform independent, high performance, ANSI C component to implementation of DLMS/COSEM (Device Language Message Specification). This allows you to build applications (on PC, raspberry pi, micro-controllers) that act as either a DLMS Client (reading data from a smart meter) or a DLMS Server (emulating a meter).
+Check out the official Gurux [DLMS.c repo](https://www.github.com/Gurux/GuruxDLMS.c) repository, and their [website](https://www.gurux.fi).
+
+## Motivation
+
+This is an un-official arduino library for the Gurux DLMS.c.
+
+## Usage
+
+1. Install library from Arduino IDE (1.5+)
+2. Include the library in you code
+3. Define your `gxignore.h` file, it should define all the stuff you want to trim off the DLMS.c library, and make sure to include it in your code before the library include
+
+    ```c
+    #include "gxignore.h"
+    #include "gx_dlms_c.h"
+    ```
+
---
+++ src/gx_dlms_c.h
@@ -0,0 +1,72 @@
+#ifndef GX_DLMS_C_H_
+#define GX_DLMS_C_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+#define GX_DLMS_MICROCONTROLLER
+#define DLMS_IGNORE_MALLOC
+
+
+#include "include/apdu.h"
+#include "include/asn1Parser.h"
+#include "include/bigInteger.h"
+#include "include/bitarray.h"
+#include "include/bytebuffer.h"
+#include "include/cipheringenums.h"
+#include "include/ciphering.h"
+#include "include/client.h"
+#include "include/converters.h"
+#include "include/cosem.h"
+#include "include/crc.h"
+#include "include/curve.h"
+#include "include/datainfo.h"
+#include "include/date.h"
+#include "include/dlms.h"
+#include "include/dlmssettings.h"
+#include "include/eccPoint.h"
+#include "include/enums.h"
+#include "include/errorcodes.h"
+#include "include/gx509Certificate.h"
+#include "include/gxaes.h"
+#include "include/gxarray.h"
+#include "include/gxdefine.h"
+#include "include/gxecdsa.h"
+#include "include/gxget.h"
+#include "include/gxignore.h"
+#include "include/gxint.h"
+#include "include/gxinvoke.h"
+#include "include/gxkey.h"
+#include "include/gxmd5.h"
+#include "include/gxmem.h"
+#include "include/gxobjects.h"
+#include "include/gxPkcs10.h"
+#include "include/gxserializer.h"
+#include "include/gxset.h"
+#include "include/gxsetignoremalloc.h"
+#include "include/gxsetmalloc.h"
+#include "include/gxsha1.h"
+#include "include/gxsha256.h"
+#include "include/gxsha384.h"
+#include "include/gxvalueeventargs.h"
+#include "include/helpers.h"
+#include "include/lnparameters.h"
+#include "include/message.h"
+#include "include/notify.h"
+#include "include/objectarray.h"
+#include "include/parameters.h"
+#include "include/privateKey.h"
+#include "include/publicKey.h"
+#include "include/replydata.h"
+#include "include/serverevents.h"
+#include "include/server.h"
+#include "include/shamirs.h"
+#include "include/snparameters.h"
+#include "include/variant.h"
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif // GX_DLMS_C_H_
